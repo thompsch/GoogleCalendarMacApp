d40ee26e5fb4bab0d18b035de92a9d14a8f383d1
Caleb Thompson <caleb.thompson@mongodb.com>
2022-10-20 10:59:46 -0700

    spike 1

---
+++ GoogleCalendar/ViewController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+using WebKit;
+
+namespace GoogleCalendar
+{
+    public partial class ViewController : NSViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var url = new NSUrl("https://calendar.google.com");
+            var request = new NSUrlRequest(url);
+            var wvd = new WebViewDelegate();
+            webView.NavigationDelegate = wvd;
+
+
+            webView.LoadRequest(request);
+            /* webView.CallAsyncJavaScript(
+                 "window.location.href='http://google.com'",
+                 null, null, WKContentWorld.DefaultClient, null);
+            */
+
+        }
+
+        public override NSObject RepresentedObject
+        {
+            get
+            {
+                return base.RepresentedObject;
+            }
+            set
+            {
+                base.RepresentedObject = value;
+            }
+        }
+    }
+}
---
+++ GoogleCalendar/ViewController.designer.cs
@@ -0,0 +1,27 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace GoogleCalendar
+{
+    [Register("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        WebKit.WKWebView webView { get; set; }
+
+        void ReleaseDesignerOutlets()
+        {
+            if (webView != null)
+            {
+                webView.Dispose();
+                webView = null;
+            }
+        }
+    }
+}
